28f14d739ef6d7b82a964ab5cb501a124cfd2d59
lamari695 <124678510+lamari695@users.noreply.github.com>
2026-03-22 15:09:08 +0000

    Add files via upload

---
+++ Main_application.js
@@ -0,0 +1,33 @@
+function Main (){
+let main = (<div>
+<table id='display'>
+<tr><input></input></tr>
+<tr><td><button id='all'>All</button></td>
+<td><button id='pending'>pending</button></td>
+<td><button id='complete'>complete</button></td>
+<td><button id='clear'>clear</button></td>
+<td><button id='past'>past</button></td>
+
+
+
+
+
+</tr>
+<tr>
+<ul>
+
+
+
+
+</ul>
+
+</tr>
+
+</table>
+
+
+
+</div>)
+
+
+}
